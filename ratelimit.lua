c114c650243f59df59faf4a9cfe1f904bd66e4d5
Bhalchandra Agashe <bagashe@users.noreply.github.com>
2021-07-24 23:23:45 -0700

    Update ratelimit.lua

--- ratelimit.lua
+++ ratelimit.lua
@@ -1,38 +1,3 @@
---[[
-A lua script extension for Redis that provides smooth, configurable & space-efficient rate limiting.
-
-Configuration example:
-	key = "api-access"
- 	value = {"1000", "3600", "300"}
-Each `actor` will be rate limited to `1000` `api-access` events per `3600 seconds`. Once the limit
-is reached, the `actor` will be locked out for `300 seconds`. Note that the rate limit applies over
-a rolling window.
-
-
-This rate-limiting solution -
-1. Does not use buckets & does not enumerate events. So it is space-efficient. It stores everything
-in a three-tuple for each `actor`,`event` pair.
-2. Uses a simple linear decay function to compute available quota. So it is blazing fast.
-3. Can be easily configured to rate limit over a few seconds or a few hours or a few days.
-
-Usage:
-  evalsha <SHA> 2 api-access <actor-id> <current-timestamp-seconds-since-epoch>
-
-The script returns the available quota as a fraction. So when the rate limit is reached, it
-returns "0".
-
-Performance:
-  Measured on processor: Intel® Core™ i7-8550U CPU @ 1.80GHz × 8
-	Using:
-	  redis-benchmark -n 100000 evalsha <SHA> api-access `shuf -i 200-300 -n 1`, `date +%s`
-	Results:
-		116550.12 requests per second with 99% of them being served in under 0.6 ms
-
-	Caveat: Client and server were running on the same host. Even so, it proves that performance of
-this Lua script should not be an issue.
-
---]]
-
 local config_identifier = KEYS[1]
 local actor_identifier = KEYS[2]
 local timestamp = tonumber(ARGV[1])
