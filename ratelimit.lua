d984b6d44e45627a6df8e37ca1573ccc705367d0
Bhal Agashe <bhalchandra@gmail.com>
2021-07-24 23:42:37 -0700

    Tabs vs Spaces :)

--- ratelimit.lua
+++ ratelimit.lua
@@ -3,7 +3,7 @@ local actor_identifier = KEYS[2]
 
 local config = redis.call('lrange', config_identifier, 0, -1)
 if not next(config) then
-	return redis.error_reply("No config found for event type - "..config_identifier)
+  return redis.error_reply("No config found for event type - "..config_identifier)
 end
 
 local max_allowed = tonumber(config[1])
@@ -18,34 +18,34 @@ local tuple = redis.call('lrange', key, 0, -1)
 -- Tuple format = {last_updated_score, last_updated_timestamp, last_blocked_timestamp}
 
 if not next(tuple) then
-	y = 1
-	redis.call('rpush', key, y)
-	redis.call('rpush', key, t1)
-	redis.call('rpush', key, 0)
+  y = 1
+  redis.call('rpush', key, y)
+  redis.call('rpush', key, t1)
+  redis.call('rpush', key, 0)
 else
-	y = tonumber(tuple[1])
-	local t0 = tonumber(tuple[2])
-	local b = tonumber(tuple[3])
+  y = tonumber(tuple[1])
+  local t0 = tonumber(tuple[2])
+  local b = tonumber(tuple[3])
 
-	if t1 - b > lockout_interval then
-	-- If not in the lockout interval since the last block
-		y = y - (max_allowed / over_interval) * (t1 - t0)
+  if t1 - b > lockout_interval then
+    -- If not in the lockout interval since the last block
+    y = y - (max_allowed / over_interval) * (t1 - t0)
 
-		if y < 0 then
-			y = 0
-		end
+    if y < 0 then
+      y = 0
+    end
 
-		y = y + 1
+    y = y + 1
 
-		if y > max_allowed then
-			y = max_allowed
-			-- Set t1 as the last_blocked_timestamp
-		  redis.call('lset', key, 2, t1)
-		end
+    if y > max_allowed then
+      y = max_allowed
+      -- Set t1 as the last_blocked_timestamp
+      redis.call('lset', key, 2, t1)
+    end
 
-		redis.call('lset', key, 0, y)
-		redis.call('lset', key, 1, t1)
-	end
+    redis.call('lset', key, 0, y)
+    redis.call('lset', key, 1, t1)
+  end
 end
 
 return tostring(1.0 - y / max_allowed)
